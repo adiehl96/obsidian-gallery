a8c9e783d2187371e18db90d9130f68802bb9a97
Tom Norris <D.Tom.Norris@gmail.com>
2023-09-09 13:14:12 -0700

    Changing the way I dev so I have a push script to push the runtime into my obsidian instance now

---
+++ PushToProject.bat
@@ -0,0 +1,3 @@
+COPY "main.js" "../PluginDev/.obsidian/plugins/tagged-gallery"
+COPY "styles.css" "../PluginDev/.obsidian/plugins/tagged-gallery"
+COPY "manifest.json" "../PluginDev/.obsidian/plugins/tagged-gallery"
